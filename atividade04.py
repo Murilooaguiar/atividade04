155e4558093bd07b484946487c97a5189a9adc0c
Murilooaguiar <146784737+Murilooaguiar@users.noreply.github.com>
2023-10-11 11:02:24 -0300

    atividade04

---
+++ atividade04.py
@@ -0,0 +1,6 @@
+#Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+numero=int(input("digite um numero:"))
+antecessor=numero-1
+import time
+time.sleep(1) 
+print("o antecessor de",numero,"e", antecessor)
